9b5c4b21131bc7aaa85ec0776da0c6303dba364c
Luís Miranda <luis.miranda@brandit.pt>
2025-11-11 17:05:55 +0000

    added loggin prefix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'St. Anthony',
+    }),
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
